6605bfc5e8fe7fe66f631269f7b98b9a1389f2b7
ssatoh17 <ssatoh17@gmail.com>
2018-01-04 09:49:00 +0900

    Update kintoneMyCommon.js

--- docs/kintoneMyCommon.js
+++ docs/kintoneMyCommon.js
@@ -30,7 +30,7 @@ jQuery.noConflict();
             //アプリ「アプリ一覧」のレコードのうち、アプリIDが appId(自身のアプリID) のレコードの詳細画面を開く
             //一覧画面の上部に追加（本当は、「・・・」のメニューの中に追加したい
             //$('.gaia-argoui-app-toolbar').append('<a target="_blank" href="https://musashi.cybozu.com/k/guest/47/'+アプリ一覧アプリid+'/show#record='+レコードID+'">app更新履歴他２</a>');
-            $('.gaia-argoui-app-toolbar').append('<a target="_blank" href="https://musashi.cybozu.com/k/guest/47/'+アプリ一覧アプリid+'/show#record='+レコードID+'" title="アプリ更新情報、アプリ概要等を表示します。"><img src="https://ssatoh17.github.io/cdn/img/Info-icon.png" style="height:20px;" alt="アプリ更新情報、アプリ概要等を表示します。"></a>');           
+            $('.gaia-argoui-app-toolbar').append('<a target="_blank" href="https://musashi.cybozu.com/k/guest/47/'+アプリ一覧アプリid+'/show#record='+レコードID+'" title="アプリ更新情報、アプリ概要等を表示します。" style="margin-bottom:14px;"><img src="https://ssatoh17.github.io/cdn/img/Info-icon.png" style="height:20px;" alt="アプリ更新情報、アプリ概要等を表示します。"></a>');           
             $('.gaia-argoui-app-toolbar').prepend('<a target="_blank" href="https://musashi.cybozu.com/k/guest/47/'+アプリ一覧アプリid+'/show#record='+レコードID+'" title="アプリ更新情報、アプリ概要等を表示します。"><img src="https://ssatoh17.github.io/cdn/img/Info-icon.png" style="height:20px;"></a>');           
             $('.gaia-argoui-pulldown').prepend('<a target="_blank" href="https://musashi.cybozu.com/k/guest/47/'+アプリ一覧アプリid+'/show#record='+レコードID+'" title="アプリ更新情報、アプリ概要等を表示します。">app更新履歴他</a>');
             $('.gaia-argoui-pulldown').append('<a target="_blank" href="https://musashi.cybozu.com/k/guest/47/'+アプリ一覧アプリid+'/show#record='+レコードID+'" title="アプリ更新情報、アプリ概要等を表示します。">app更新履歴他</a>');
