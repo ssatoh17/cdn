f83dc77316dbb7f40afef5a6a923985c6451799e
ssatoh17 <ssatoh17@gmail.com>
2018-01-04 15:08:32 +0900

    Update kintoneMyCommon.js

--- docs/kintoneMyCommon.js
+++ docs/kintoneMyCommon.js
@@ -1,11 +1,12 @@
 jQuery.noConflict();
 var isGuestSpace=false;
 if(location.href.indexOf("/guest/")!=-1)isGuestSpace = true;
+console.log("ゲストスペースか否か" + isGuestSpace);
 
 (function($) {
    "use strict";
    kintone.events.on("app.record.index.show", function(e) {
-      var 更新日時等 = 'ver0.910 共通JS更新日時 平成30年1月4日 15時07分';
+      var 更新日時等 = 'ver0.910 共通JS更新日時 平成30年1月4日 15時08分';
         $('body').append('<div>'+更新日時等+'</div>');
         console.info("共通Javascript更新日時＝"+更新日時等);
 
