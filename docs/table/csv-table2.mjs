ae824b138f0457c7ad512e057efb8c8694535b62
ssatoh17 <ssatoh17@gmail.com>
2021-12-20 02:41:15 +0900

    theadとtbody要素を追加

--- docs/table/csv-table2.mjs
+++ docs/table/csv-table2.mjs
@@ -23,8 +23,8 @@ class CSVTable extends HTMLElement {
     const tbl = c("table");
     let firstline = true;
     let maxTdNum = 2;
-    // let thead = c("thead");
-    // let tbody = c("tbody");
+    let thead = c("thead");
+    let tbody = c("tbody");
     for (const s of ss) {
       const tr = c("tr");
       tbl.append(tr);
@@ -44,9 +44,16 @@ class CSVTable extends HTMLElement {
         const td = c("td");
         tr.appendChild(td);
       }
-      tbl.appendChild(tr);
+      if(firstline){          
+        thead.appendChild(tr);
+      }else{
+        tbody.appendChild(tr);
+      }
+      // tbl.appendChild(tr);
       firstline = false;
     }
+    tbl.appendChild(thead);
+    tbl.appendChild(tbody);
     this.appendChild(tbl);
 
     // css
